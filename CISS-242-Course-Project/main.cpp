5bf6795998233d0188876a9da75a4a2983a66665
John Carty <john.carty@gmail.com>
2015-10-05 21:07:26 -0400

    Initial Commit

---
+++ CISS-242-Course-Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CISS-242-Course-Project
+//
+//  Created by John Carty on 10/5/15.
+//  Copyright © 2015 John Carty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
